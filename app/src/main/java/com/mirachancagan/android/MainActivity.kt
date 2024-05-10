4a65f2ca72ff075a6fe3be4b98c9de1a045ee564
Miraçhan <120271540+mirachanc@users.noreply.github.com>
2024-05-10 23:10:40 +0300

    login activity created

--- app/src/main/java/com/mirachancagan/android/MainActivity.kt
+++ app/src/main/java/com/mirachancagan/android/MainActivity.kt
@@ -2,10 +2,29 @@ package com.mirachancagan.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.mirachancagan.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun loginUser(view: View) {
+        val email = binding.editTextTextEmailAddress.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
+        if(email.isEmpty()){
+            ///show error dialog
+        } else if(password.isEmpty()){
+            ///show error dialog
+        } else {
+            ///login using firebase auth
+        }
     }
 }
